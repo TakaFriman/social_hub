3af81c92095bb4319dfd438a1d46e456a237277a
Geor Tebiev <geor.tebiev@mail.ru>
2024-07-31 16:08:56 +0300

    new files

---
+++ lib/screens/auth/register/view/register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+@RoutePage()
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('sss'),
+    );
+  }
+}
